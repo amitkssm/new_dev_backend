6102bddb00383a315b8804ab912e36e17aa362b1
Amit Kumar <QD2442@QDS.COM>
2025-07-14 15:34:48 +0530

    add vercel file

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
-
 const mongoose = require("mongoose");
 const express = require("express");
-const fs = require('fs')
-const jwt = require('jsonwebtoken');
+const fs = require("fs");
+const jwt = require("jsonwebtoken");
 const multer = require("multer");
-var bcrypt = require('bcryptjs');
-var randomstring = require('randomstring');
-const bodyParser = require('body-parser');
-var moment = require('moment');
-const path = require('path');
-const https = require('https');
-const serverless = require('serverless-http');
-
-
-
-const ObjectId = require('mongoose').Types.ObjectId;
+const bcrypt = require("bcryptjs");
+const randomstring = require("randomstring");
+const bodyParser = require("body-parser");
+const moment = require("moment");
+const path = require("path");
+const https = require("https");
+const serverless = require("serverless-http");
 const cors = require("cors");
 
-
-// Connect to MongoDB
 require("./db/config");
-
 
 const app = express();
 app.use(cors());
@@ -28,10 +20,8 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-app.get('/file/uploads/:path', (req, res) => {
-    console.log("DSDA")
-    const safePath = path.join(__dirname, 'uploads', req.params.path);
+app.get("/file/uploads/:path", (req, res) => {
+    const safePath = path.join(__dirname, "uploads", req.params.path);
     fs.readFile(safePath, (err, data) => {
         if (err) {
             console.error(err);
@@ -41,44 +31,15 @@ app.get('/file/uploads/:path', (req, res) => {
     });
 });
 
-
-// api/hello.js
-module.exports = (req, res) => {
-  res.status(200).json({ message: "Hello from Vercel!" });
-};
-
-
 // Serve uploads statically
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-const adminRoute = require("./admin/index")
-const frontUserRoute = require("./frontUsers/index")
+const adminRoute = require("./admin/index");
+const frontUserRoute = require("./frontUsers/index");
 
-/************************ API Rout ******************* */
-app.use("/admin",adminRoute);
+app.use("/admin", adminRoute);
+app.use("/front_user", frontUserRoute);
 
-app.use("/front_user",frontUserRoute);
-
-
-
-// app.listen((3007), () => {
-//     console.log("app is running on port 3007")
-// })
-
-// Export handler
+// ✅ This is the correct way for Vercel Serverless
+module.exports = app;
 module.exports.handler = serverless(app);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
